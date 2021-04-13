3d17694e88671ea60a68f4ad6a80bf62400486e2
Samuel M <samuel.morris2@mail.mcgill.ca>
2021-04-13 19:04:59 -0400

    Add Heavy Rollout Players to test

---
+++ src/student_player/NormalMCTSPlayer.java
@@ -0,0 +1,32 @@
+package student_player;
+
+import boardgame.Move;
+
+import pentago_twist.PentagoPlayer;
+import pentago_twist.PentagoBoardState;
+
+/**
+ * 
+ * @author Samuel Morris (dodobird)
+ *
+ */
+public class NormalMCTSPlayer extends PentagoPlayer {
+
+	/**
+	 * NOTE: Cannot change this function at all.
+	 */
+    public NormalMCTSPlayer() {
+        super("NormalMCTSPlayer");
+    }
+
+    /**
+     * This is the primary method that you need to implement. The ``boardState``
+     * object contains the current state of the game, which your agent must use to
+     * make decisions.
+     */
+    public Move chooseMove(PentagoBoardState boardState) {
+        return MyTools.MonteCarloTreeSearch(boardState, false);
+    }
+    
+    
+}
